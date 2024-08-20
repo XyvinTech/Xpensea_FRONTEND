b84c04eb07fbae4c42cd683410f62582005f34d2
anjanavu <anjanavu2000@gmail.com>
2024-08-20 11:58:01 +0530

    icon

--- src/components/approvals/Details.jsx
+++ src/components/approvals/Details.jsx
@@ -4,15 +4,16 @@ import { ApproveIcon } from "../../assets/icons/ApproveIcon";
 
 const Details = ({ data }) => {
   return (
-    <Stack  bgcolor={"#fff"} borderRadius={"12px"} paddingBottom={4}minHeight={'319px'}>
-      <Typography
-        padding={3}
-        color={"#002B9B"}
-        variant="h3"
-        fontWeight={600}
-      >
+    <Stack
+      bgcolor={"#fff"}
+      borderRadius={"12px"}
+      paddingBottom={4}
+      minHeight={"319px"}
+    >
+      <Typography padding={3} color={"#002B9B"} variant="h3" fontWeight={600}>
         Details
-      </Typography> <Divider sx={{ width: "100%" }} />
+      </Typography>{" "}
+      <Divider sx={{ width: "100%" }} />
       <Stack padding={2} direction={"row"} justifyContent={"space-between"}>
         <Stack direction="row" spacing={1}>
           <ApproveIcon />
@@ -22,23 +23,26 @@ const Details = ({ data }) => {
             </Typography>
             <Typography variant="h11" color="#002B9B" fontWeight={"600"}>
               Rs {data?.totalAmount}
-              <Typography variant="h3">
-              {data?.createdAt}
-              </Typography>
+              <Typography variant="h3">{data?.createdAt}</Typography>
             </Typography>
-            <Typography variant="h3" >
-                Created By {data?.creator?.name}
+            {data?.creator?.name && (
+              <Typography variant="h3">
+                Created By: {data?.creator?.name}
               </Typography>
+            )}
+            {data?.start && (
+              <Typography variant="h3">Started on : {data?.start}</Typography>
+            )}{" "}
+            {data?.end && (
+              <Typography variant="h3">Ended on : {data?.end}</Typography>
+            )}
           </Stack>
         </Stack>
         <Stack spacing={2} justifyContent="center">
-       
           <Typography variant="h3" color={"#333333"}>
-         {data?.reportId}
+            {data?.reportId}
           </Typography>
-          <Typography variant="h3" color={"#B4B4B4"}>
-        
-          </Typography>
+          <Typography variant="h3" color={"#B4B4B4"}></Typography>
         </Stack>
       </Stack>
     </Stack>
