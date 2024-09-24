526846e2d4738658dcf0942f1ee80391312c4912
anjanavu <anjanavu2000@gmail.com>
2024-09-24 19:44:24 +0530

    bugs

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -472,7 +472,7 @@ const Layout = (props) => {
                   marginRight: "5px",
                 }}
               >
-                4 minutes ago
+                0 minutes ago
               </span>{" "}
               <SyncIcon />
             </Typography>
