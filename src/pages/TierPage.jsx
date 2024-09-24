490d5d25a67a780c0b14197f92e0df1a2d36751c
anjanavu <anjanavu2000@gmail.com>
2024-09-24 19:43:52 +0530

    bugs

--- src/pages/TierPage.jsx
+++ src/pages/TierPage.jsx
@@ -60,7 +60,7 @@ const TierPage = () => {
   const userColumns = [
     { title: "Tier Title", field: "title", sortable: false },
     { title: "Date", field: "activationDate", sortable: true },
-    { title: "No of Employe", field: "noOfEmployees", sortable: true },
+    { title: "No of Employee", field: "noOfEmployees", sortable: true },
     { title: "Max Amount", field: "totalAmount", sortable: true },
     { title: "No of allowance", field: "noAllowance", sortable: true },
   ];
